630e4c743a725239c5dd3263bc07d4c8b9719370
nithin <nithinragutla4@gmail.com>
2025-03-20 00:48:31 +0530

    new commit

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
